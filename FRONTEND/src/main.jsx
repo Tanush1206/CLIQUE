2750349ffaa9318502aad0e4ca4882bd053761c3
tanush1206 <tanushthakran1206@gmail.com>
2025-10-15 20:38:13 +0530

    comments

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 import ScrollToTop from "@/components/ScrollToTop.jsx";
 import { AuthProvider } from "@/hooks/useAuth.jsx";
 
-
+//3123
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
